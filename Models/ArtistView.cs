8abb8b02a0319c22c098419026d4c7a107dd248d
antony <antony.collin.d@gmail.com>
2022-05-22 14:12:46 -0400

    pop

---
+++ Models/ArtistView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MySpace.Models
+{
+    public partial class Artist
+    {
+        public Artist()
+        {
+            FanLikes = new HashSet<FanLike>();
+            Messages = new HashSet<Message>();
+            Videos = new HashSet<Video>();
+        }
+    }
+}
